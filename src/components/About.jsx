12b0956486e4a245c5785c1dd3b5cef76a9d58dc
Anthony Feng <afeng1122@gmail.com>
2024-03-23 13:59:56 +1100

    Initial description in about page added.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm an upcoming software developer with experience using TypeScript,
+        JavaScript, expertise in utilizing frameworks such as React.js, Node.js
+        and Three.js. I also have experience using Python and I'm currently
+        learning C# as well. Whilst I'm currently relatively new, I'm a quick
+        learner and can collaborate with clients and other team members to
+        develop efficient and user-friendly solutions, I more than happy to work
+        with others to bring ideas to life!
+      </motion.p>
+    </>
+  );
+};
 
-export default About
+export default About;
