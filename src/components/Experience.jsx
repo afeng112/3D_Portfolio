0681d6be164a95367dfcf22001bdb2e51c52748d
Anthony Feng <afeng1122@gmail.com>
2024-03-25 14:15:02 +1100

    Added workplace experience section.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,7 +3,7 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-import motion from "react-motion";
+import { motion } from "framer-motion";
 import "react-vertical-timeline-component/style.min.css";
 
 import { styles } from "../styles";
@@ -11,8 +11,51 @@ import { experiences } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
+// Create new experience card
+const ExperienceCard = ({ experience }) => (
+  <VerticalTimelineElement
+    // Set styling of the cards
+    contentStyle={{ background: "#1d1836", color: "#fff" }}
+    contentArrowStyle={{ borderRight: "7px solid #232631" }}
+    // Set styling of the date to the right of the icons.
+    date={experience.date}
+    // Print out icon, change background for every 2nd workplace.
+    iconStyle={{ background: experience.iconBg }}
+    icon={
+      <div>
+        <img
+          src={experience.icon}
+          alt={experience.company_name}
+          className="w-[60%] h-[60%] object-contain"
+        />
+      </div>
+    }
+  >
+    <div>
+      {/* Prints out title for each workplace. */}
+      <h3 className="text-white text-[24px] font-hold">{experience.title}</h3>
+    </div>
+  </VerticalTimelineElement>
+);
+
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <>
+      {/* Create experience section */}
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My journey thus far...</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+      <div className="mt-20 flex flex-col">
+        {/* Using the vertical timeline module, print out my workplace experiences. */}
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
 };
 
-export default Experience;
+export default SectionWrapper(Experience, "Work");
